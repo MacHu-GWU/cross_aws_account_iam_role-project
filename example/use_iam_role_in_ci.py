8928d78ea83218cd601416c6ad8e47c570b5ccf6
Sanhe <MacHu-GWU@users.noreply.github.com>
2023-11-30 13:21:05 -0500

    test

--- example/use_iam_role_in_ci.py
+++ example/use_iam_role_in_ci.py
@@ -13,50 +13,56 @@ prefix = "a1b2-"
 grantee_1_bsm = BotoSesManager(profile_name="bmt_app_dev_us_east_1")
 iam_role_arn = x_aws_acc.IamRoleArn(
     account=grantee_1_bsm.aws_account_id,
-    name="cross-aws-account-iam-role-github-open-id-connection",
+    name="cross-aws-account-iam-role-github-oidc",
 )
 grantee_1 = x_aws_acc.Grantee(
     bsm=grantee_1_bsm,
-    stack_name=f"{prefix}cross-aws-account-iam-role-oidc-grantee",
+    stack_name=f"{prefix}cross-aws-account-iam-role-github-oidc-grantee",
     iam_arn=iam_role_arn,
     policy_name=f"{prefix}cross_aws_account_iam_role_github_oidc-grantee",
 )
 
-owner_1_bsm = BotoSesManager(profile_name="bmt_app_test_us_east_1")
-owner_1 = x_aws_acc.Owner(
-    bsm=owner_1_bsm,
-    stack_name=f"{prefix}cross-aws-account-iam-role-github-oidc-test-owner",
-    role_name=f"{prefix}cross_aws_account_iam_role_github_oidc_test_owner",
-    policy_name=f"{prefix}cross_aws_account_iam_role_github_oidc_test_owner",
-    policy_document={
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Action": [
-                    "iam:ListAccountAliases",
-                    "sts:GetCallerIdentity",
-                ],
-                "Resource": "*",
-            },
-        ],
-    },
-)
-
-owner_1.grant(grantee_1)
+owner_list = list()
+for aws_profile, env_name in [
+    ("bmt_app_dev_us_east_1", "dev"),
+    ("bmt_app_test_us_east_1", "test"),
+    ("bmt_app_prod_us_east_1", "prod"),
+]:
+    owner_bsm = BotoSesManager(profile_name=aws_profile)
+    owner = x_aws_acc.Owner(
+        bsm=owner_bsm,
+        stack_name=f"{prefix}cross-aws-account-iam-role-github-oidc-{env_name}-owner",
+        role_name=f"{prefix}cross_aws_account_iam_role_github_oidc_{env_name}_owner",
+        policy_name=f"{prefix}cross_aws_account_iam_role_github_oidc_{env_name}_owner",
+        policy_document={
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Action": [
+                        "iam:ListAccountAliases",
+                        "sts:GetCallerIdentity",
+                    ],
+                    "Resource": "*",
+                },
+            ],
+        },
+    )
+    owner.grant(grantee_1)
+    owner_list.append(owner)
 
 
 def deploy():
     x_aws_acc.deploy(
         grantee_list=[grantee_1],
-        owner_list=[owner_1],
+        owner_list=owner_list,
     )
 
 
 def delete():
     x_aws_acc.delete(
         grantee_list=[grantee_1],
-        owner_list=[owner_1],
+        owner_list=owner_list,
     )
 
 
--- test_cross_aws_account_iam_role.py
+++ test_cross_aws_account_iam_role.py
@@ -8,13 +8,18 @@ def print_caller_identity(bsm: BotoSesManager):
     parts = res["Arn"].split(":")
     parts[4] = parts[4][:2] + "********" + parts[4][-2:]
     arn = ":".join(parts)
-    print(f"    now we are using principal {arn}")
+    print(f"  now we are using principal {arn}")
 
+
+prefix = "a1b2-"
 bsm = BotoSesManager()
+print("at begin:")
 print_caller_identity(bsm)
-
-bsm_assumed = bsm.assume_role(
-    role_arn=f"arn:aws:iam::{bsm.aws_account_id}:role/cross-aws-account-iam-role-github-oidc-owner",
-    role_session_name="sample_role_session",
-)
-print_caller_identity(bsm_assumed)
+print("validate cross account permission ...")
+for env_name in ["dev", "test", "prod"]:
+    role_name = f"{prefix}cross_aws_account_iam_role_github_oidc_{env_name}_owner"
+    bsm_assumed = bsm.assume_role(
+        role_arn=f"arn:aws:iam::{bsm.aws_account_id}:role/{role_name}",
+        role_session_name="sample_role_session",
+    )
+    print_caller_identity(bsm_assumed)
