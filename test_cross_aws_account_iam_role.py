543682eaedc8bb41970cd107766611299b605d92
Sanhe <MacHu-GWU@users.noreply.github.com>
2023-11-30 13:28:21 -0500

    fix ci

--- test_cross_aws_account_iam_role.py
+++ test_cross_aws_account_iam_role.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+"""
+This script tests the cross account AWS permission in GitHub action without
+showing the AWS Account id.
+"""
+
+import os
 from boto_session_manager import BotoSesManager
 
 
@@ -17,9 +23,10 @@ print("at begin:")
 print_caller_identity(bsm)
 print("validate cross account permission ...")
 for env_name in ["dev", "test", "prod"]:
+    aws_account_id = os.environ[f"{env_name.upper()}_AWS_ACCOUNT_ID"]
     role_name = f"{prefix}cross_aws_account_iam_role_github_oidc_{env_name}_owner"
     bsm_assumed = bsm.assume_role(
-        role_arn=f"arn:aws:iam::{bsm.aws_account_id}:role/{role_name}",
+        role_arn=f"arn:aws:iam::{aws_account_id}:role/{role_name}",
         role_session_name="sample_role_session",
     )
     print_caller_identity(bsm_assumed)
