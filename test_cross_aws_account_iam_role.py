6d8d827bd5c58987f6e2f686d4427542ff7c6eb5
Sanhe <MacHu-GWU@users.noreply.github.com>
2023-11-30 12:45:54 -0500

    Update test_cross_aws_account_iam_role.py

--- test_cross_aws_account_iam_role.py
+++ test_cross_aws_account_iam_role.py
@@ -2,13 +2,19 @@
 
 from boto_session_manager import BotoSesManager
 
+
+def print_caller_identity(bsm: BotoSesManager):
+    res = bsm.sts_client.get_caller_identity()
+    parts = res["Arn"].split(":")
+    parts[4] = parts[4][:2] + "********" + parts[4][-2:]
+    arn = ":".join(parts)
+    print(f"    now we are using principal {arn}")
+
 bsm = BotoSesManager()
+print_caller_identity(bsm)
+
 bsm_assumed = bsm.assume_role(
     role_arn=f"arn:aws:iam::{bsm.aws_account_id}:role/cross-aws-account-iam-role-github-oidc-owner",
     role_session_name="sample_role_session",
 )
-res = bsm_assumed.sts_client.get_caller_identity()
-parts = res["Arn"].split(":")
-parts[4] = parts[4][:2] + "********" + parts[4][-2:]
-arn = ":".join(parts)
-print(f"    now we are using principal {arn}")
+print_caller_identity(bsm_assumed)
